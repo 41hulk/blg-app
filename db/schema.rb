89ced03d71fd66a5cec475cf3a6a5d82f00652fb
Ntare Guy <gntare2@gmail.com>
2022-09-17 15:48:01 +0200

    Change var from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_125519) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string "text"
+    t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
@@ -34,8 +34,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_125519) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.string "text"
+    t.text "title"
+    t.text "text"
     t.integer "comments_counter", default: 0
     t.integer "likes_counter", default: 0
     t.datetime "created_at", null: false
@@ -45,9 +45,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_17_125519) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.string "bio"
+    t.text "name"
+    t.text "photo"
+    t.text "bio"
     t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
