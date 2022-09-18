7580dcc8973f6b272bc8ca3203792f174b62bce7
Ntare Guy <gntare2@gmail.com>
2022-09-18 02:48:31 +0200

    like test

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
-
+  validates_associated :post, :author
   after_save :update_likes_counter
   # Method that updates the likes counter for a post.
   def update_likes_counter
