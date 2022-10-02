30f82f114562eb9b80507db4f71b3b714a6a579b
Ntare Guy <gntare2@gmail.com>
2022-10-02 03:57:25 +0200

    add view

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,16 +9,15 @@ class Post < ApplicationRecord
   validates :likes_counter, comparison: { greater_than_or_equal_to: 0 }
   validates :comments_counter, comparison: { greater_than_or_equal_to: 0 }
 
-  # Method that updates the posts counter for a user.
+  def find_comments
+    Comment.where(post: self).order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  # A method that updates the posts counter for a user.
   def update_posts_counter
     author.increment!(:posts_counter)
   end
 
-  private :update_posts_counter
-
-  # Method which returns the 5 most recent comments for a given post.
-
-  def find_comments
-    Comment.where(post: self).order(created_at: :desc).limit(5)
-  end
 end
